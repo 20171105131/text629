b316811c4ff0ece6472e3757a6bc843ea2a52793
20171105131 <1105197998@qq.com>
2018-06-29 19:28:44 +0800

    Initial Commit

---
+++ text629/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  text629
+//
+//  Created by 唐伟强 on 2018/6/29.
+//  Copyright © 2018年 唐伟强. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
